87976f39b9fde303301000607e4ed0f234cfe306
luffyxgoku <kinghunz12@gmail.com>
2025-05-28 16:52:10 +0530

    Lazy Loaded the RandomHadith Component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Hero from "../Hero/Hero";
 import Banner from "../Banner/Banner";
 import Services from "../Services/Services";
@@ -8,15 +8,20 @@ import "./Home.css";
 import Pillers from "./Pillers";
 import Afterlife from "../AfterLife/Afterlife";
 import Footer from "../Footer/Footer";
-import Randomhadith from "../RandomHadith/Randomhadith";
 
 export default function Home() {
+  const Randomhadith = React.lazy(() => import("../RandomHadith/Randomhadith"));
+
   return (
     <div className="main-home-container">
       <Hero />
       <Banner BannerData={BannerData} />
       <Services />
-      <Randomhadith />
+
+      <Suspense fallback="Loading...">
+        <Randomhadith />
+      </Suspense>
+
       <h1 className="essence-text">Essense of Islam</h1>
       <p className="essense-subheading">
         Core Beliefs and Practices of the Faith
